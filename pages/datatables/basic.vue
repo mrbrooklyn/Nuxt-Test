420712f4ec5ffc8e7a250c90293d108366d73c78
Brook <book.damrongdech@gmail.com>
2024-03-24 13:19:59 +0700

    Update Tailwind & Add Basic - Data Tables

---
+++ pages/datatables/basic.vue
@@ -0,0 +1,80 @@
+<template>
+    <div>
+        <div class="panel flex items-center overflow-x-auto whitespace-nowrap p-3 text-primary">
+            <div class="rounded-full bg-primary p-1.5 text-white ring-2 ring-primary/30 ltr:mr-3 rtl:ml-3">
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-3.5 w-3.5">
+                    <path
+                        d="M19.0001 9.7041V9C19.0001 5.13401 15.8661 2 12.0001 2C8.13407 2 5.00006 5.13401 5.00006 9V9.7041C5.00006 10.5491 4.74995 11.3752 4.28123 12.0783L3.13263 13.8012C2.08349 15.3749 2.88442 17.5139 4.70913 18.0116C9.48258 19.3134 14.5175 19.3134 19.291 18.0116C21.1157 17.5139 21.9166 15.3749 20.8675 13.8012L19.7189 12.0783C19.2502 11.3752 19.0001 10.5491 19.0001 9.7041Z"
+                        stroke="currentColor"
+                        stroke-width="1.5"
+                    />
+                    <path
+                        opacity="0.5"
+                        d="M7.5 19C8.15503 20.7478 9.92246 22 12 22C14.0775 22 15.845 20.7478 16.5 19"
+                        stroke="currentColor"
+                        stroke-width="1.5"
+                        stroke-linecap="round"
+                    />
+                </svg>
+            </div>
+            <span class="ltr:mr-3 rtl:ml-3">Documentation: </span>
+            <a href="https://www.npmjs.com/package/@bhplugin/vue3-datatable" target="_blank" class="block hover:underline">
+                https://www.npmjs.com/package/@bhplugin/vue3-datatable
+            </a>
+        </div>
+        <div class="panel flex items-center overflow-x-auto whitespace-nowrap p-3 text-primary">
+            <div class="panel mt-6 pb-0">
+                <h5 class="mb-5 text-lg font-semibold dark:text-white-light">Basic</h5>
+                <vue3-datatable   vue3-datatable :rows="rows" :columns="cols"> </vue3-datatable>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+    import { ref } from "vue";
+    import Vue3Datatable from "@bhplugin/vue3-datatable";
+    import "@bhplugin/vue3-datatable/dist/style.css";
+
+    const cols = ref([
+      { field: "id", title: "ID", width: "90px", filter: false },
+      { field: "name", title: "Name" },
+      { field: "username", title: "Username" },
+      { field: "email", title: "Email" },
+      { field: "phone", title: "Phone" },
+      { field: "date", title: "Date", type: "date" },
+      { field: "active", title: "Active", type: "bool" },
+      { field: "age", title: "Age", type: "number" },
+      { field: "address.city", title: "Address" },
+      { field: "company.name", title: "Company" },
+    ]);
+
+    const rows = ref([
+      {
+        id: 1,
+        name: "Leanne Graham",
+        username: "Bret",
+        email: "Sincere@april.biz",
+        address: {
+          street: "Kulas Light",
+          suite: "Apt. 556",
+          city: "Gwenborough",
+          zipcode: "92998-3874",
+          geo: {
+            lat: "-37.3159",
+            lng: "81.1496",
+          },
+        },
+        phone: "1-770-736-8031 x56442",
+        website: "hildegard.org",
+        company: {
+          name: "Romaguera-Crona",
+          catchPhrase: "Multi-layered client-server neural-net",
+          bs: "harness real-time e-markets",
+        },
+        date: "Tue Sep 27 2022 22:19:57",
+        age: 10,
+        active: true,
+      }
+    ]);
+</script>
